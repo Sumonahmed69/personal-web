029f9c89b4b2ed4f68557d0987e9387ca04a9f7b
Sumonahmed69 <sumonahmed012340@gmail.com>
2021-05-02 19:14:20 +0600

    done

--- src/components/Home/AboutMe/AboutMe.js
+++ src/components/Home/AboutMe/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <section style={{ background: '#222f3e' }} className=' py-5' >
+        <section style={{ background: '#222f3e' }}  className=' py-5' >
             <div className='container '>
                 <h1 className="text-center text-white "> About Me</h1>
                 <h5 className="mt-5 text-white"> A Front End Web Developer using React, Node.js, MongoDB, Express (MERN). Experienced in building single page applications, User Auth and oAuth with Firebase. Common libraries of use: Redux, Express, body-parser, react-router. Common use of UI libraries: Bootstrap 4, React-Bootstrap, Material-UI, Ant Design.</h5>
